9cb2ee7d83c5b2e6704c1310444bf48574e1d512
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-04-02 17:03:42 -0500

    Commit 6

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -67,6 +67,6 @@ public class Tesis extends Escrito{
 	
 	
 	public String toString() {
-		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.idea + "\n" + this.idea + "\n" + this.argumentos.length + "\n" + this.conclusion + "\n" + this.referencias;
+		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() +  "\n" + this.idea + "\n" + this.argumentos.length + "\n" + this.conclusion + "\n" + this.referencias;
 	}
 }
