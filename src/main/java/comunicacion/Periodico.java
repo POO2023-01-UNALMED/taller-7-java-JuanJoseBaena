4627e86e58976fa984737d660cc1c69b9febfadb
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-04-02 16:43:34 -0500

    Commit 4

---
+++ src/main/java/comunicacion/Periodico.java
@@ -0,0 +1,52 @@
+package comunicacion;
+
+public class Periodico extends Escrito{
+	private String fecha;
+	private String primicia;
+	private String interpretacion;
+	
+	
+	public Periodico (String origen, String titulo, String autor, int paginas, String fecha, String primicia, String interpretacion) {
+		super (origen, titulo, autor, paginas);
+		this.fecha = fecha;
+		this.primicia = primicia;
+		this.interpretacion = interpretacion;
+	}
+	
+	
+	public String getFecha() {
+		return fecha;
+	}
+	public void SetFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+	public String getPrimicia() {
+		return primicia;
+	}
+	public void setPrimicia(String primicia) {
+		this.primicia = primicia;
+	}
+	
+	
+	public String getIntepretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	public String interpretacion() {
+		return interpretacion;
+	}
+	
+	
+	public int palabrasTotales( int palabrasPagina) {
+		return 10 * getPaginas() * palabrasPagina;
+	}
+	
+	
+	public String toString() {
+		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.fecha + "\n" + this.primicia;
+	}
+}
